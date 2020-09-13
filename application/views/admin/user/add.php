957f58dccf587d62dd8b0ea944c67e6e77afa235
Thae Thae <thae91861@gmail.com>
2020-09-13 10:22:17 +0630

    added add.php

--- application/views/admin/user/add.php
+++ application/views/admin/user/add.php
@@ -171,7 +171,7 @@
                                         <div class="controls">
                                             <button type="submit" class="btn btn-success">Save user</button>
 
-                                            <button type="reset" class="btn btn-default waves-effect" data-dismiss="modal" value="Reset">Cancel</button>
+                                            <button type="reset" class="btn btn-default waves-effect"  value="cancel" onclick="window.history.back()">Cancel</button>
                                         </div>
                                     </div>
                                 </div>
