043632f061cc0dc86237e461f65904bd586e4f67
KhinNyeinChan <poe2161999@gmail.com>
2020-09-21 10:58:10 +0630

    updated index.php of laundry

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -286,20 +286,21 @@
                 <div class="navbar-header">
                     <a class="navbar-brand" href="<?php echo base_url('admin/dashboard') ?>">
                         <!-- Logo icon -->
-                        <b>
+                        <!-- <b> -->
                             <!--You can put here icon as well // <i class="wi wi-sunset"></i> //-->
                             <!-- Dark Logo icon -->
-                            <img src="<?php echo base_url() ?>assets/images/logo-icon.png" alt="homepage" class="dark-logo" />
+                            <!-- <img src="<?php //echo base_url() ?>assets/images/logo-icon.png" alt="homepage" class="dark-logo" /> -->
                             <!-- Light Logo icon -->
-                            <img src="<?php echo base_url() ?>assets/images/logo-light-icon.png" alt="homepage" class="light-logo" />
-                        </b>
+                            <!-- <img src="<?php //echo base_url() ?>assets/images/logo-light-icon.png" alt="homepage" class="light-logo" /> -->
+                        <!-- </b> -->
                         <!--End Logo icon -->
                         <!-- Logo text -->
                         <span>
                          <!-- dark Logo text -->
-                         <img src="<?php echo base_url() ?>assets/images/logo-text.png" alt="homepage" class="dark-logo" />
+                         <!-- <img src="<?php// echo base_url() ?>assets/images/logo-text.png" alt="homepage" class="dark-logo" /> -->
                          <!-- Light Logo text -->    
-                         <img src="<?php echo base_url() ?>assets/images/logo-light-text.png" class="light-logo" alt="homepage" /></span> </a>
+                         <!-- <img src="<?php //echo base_url() ?>assets/images/logo-light-text.png" class="light-logo" alt="homepage" /></span>  -->
+                         <h2 class="text-white font-weight-bold">Laundry</h2></a>
                 </div>
                 
                 <!-- End Logo -->
